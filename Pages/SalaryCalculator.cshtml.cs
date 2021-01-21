6a0a4078e504c0b0cf91f06c8766998a12f2e04a
twdeveloper <twdeveloper@outlook.com>
2021-01-21 21:24:40 +0800

    first commit

---
+++ Pages/SalaryCalculator.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace DotnetCoreWebAppDIExample.Pages
+{
+    public class SalaryCalculatorModel : PageModel
+    {
+        private HR.ISalaryFormula _SalaryFormula;
+        public SalaryCalculatorModel(HR.ISalaryFormula SalaryFormula)
+        {
+            _SalaryFormula = SalaryFormula;
+        }
+
+        public float Salary { get; set; }
+        public ActionResult OnGet()
+        {
+            HR.SalaryCalculator SalaryCalculator =
+                new HR.SalaryCalculator(_SalaryFormula);
+            Salary = SalaryCalculator.Calculate(8 * 19, 200, 8 * 1);
+            return Page();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DotnetCoreWebAppDIExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            //一般員工薪資計算
+            services.AddTransient<HR.ISalaryFormula, HR.SalaryFormula>();
+            //替換成請假不扣薪公式
+            //services.AddTransient<HR.ISalaryFormula, HR.BossSalaryFormula>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
